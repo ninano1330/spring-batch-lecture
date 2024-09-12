69f477597aae0bdd4138b6083256f6b101b85a13
mg-jeon <jmg@hunet.co.kr>
2024-09-12 16:08:27 +0900

    섹션 3 스프링 배치 시작 - Hello Spring Batch 시작하기

--- src/main/java/io/springbatch/springbatchlecture/SpringBatchLectureApplication.java
+++ src/main/java/io/springbatch/springbatchlecture/SpringBatchLectureApplication.java
@@ -1,9 +1,16 @@
 package io.springbatch.springbatchlecture;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+/**
+ * 스프링 배치 어노테이션
+ * 총 4개의 설정 클래스 실행
+ */
+@EnableBatchProcessing
 public class SpringBatchLectureApplication {
 
     public static void main(String[] args) {
