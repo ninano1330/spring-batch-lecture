a68b29be04e4db7dcedc39aae97c8619a04c32dc
mg-jeon <jmg@hunet.co.kr>
2024-09-26 15:04:22 +0900

    섹션 3 스프링 배치 시작 - DB 설정

--- src/main/java/io/springbatch/springbatchlecture/SpringBatchLectureApplication.java
+++ src/main/java/io/springbatch/springbatchlecture/SpringBatchLectureApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 /**
  * 스프링 배치 어노테이션
  * 총 4개의 설정 클래스 실행
